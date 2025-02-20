ef079b8cb26d28aaff844c0008942625988b1b86
proximacentauri11 <dfalin1993@gmail.com>
2025-02-20 19:56:21 +0300

    Update HW07_2__2.java

--- java/HW07/HW07_2__2.java
+++ java/HW07/HW07_2__2.java
@@ -5,11 +5,10 @@ public class HW07_2__2 {
             first = first + i + " ";
         }
         first = first.substring(0, first.length() - 1);
-        System.out.println(first);
 
-        String space = "  ";
+        String space = "";
 
-        for (int i = 2; i < first.length(); i = i + 2) {
+        for (int i = 0; i < first.length(); i = i + 2) {
             System.out.println(space + first.substring(0, first.length() - i));
             space = space + "  ";
         }
