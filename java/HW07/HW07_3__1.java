f21a9ee4c8662dbc2925368e7d86dfd3a8e0fbcd
proximacentauri11 <dfalin1993@gmail.com>
2025-02-20 14:07:24 +0300

    Create HW07_3__1.java

---
+++ java/HW07/HW07_3__1.java
@@ -0,0 +1,21 @@
+public class HW07_3__1 {
+    public static int addition(int a, int b) {
+        return a + b;
+    }
+    public static int substruction(int a, int b) {
+        return a - b;
+    }
+    public static int multiplication(int a, int b) {
+        return a * b;
+    }
+    public static int division(int a, int b){
+        return a / b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(addition(15,20));
+        System.out.println(substruction(40,23));
+        System.out.println(multiplication(27,8));
+        System.out.println(division(140,7));
+    }
+}
