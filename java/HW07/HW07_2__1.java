8203c0f9ad0bd345e6f504b8e2a232d82330dd14
proximacentauri11 <dfalin1993@gmail.com>
2025-02-20 19:55:57 +0300

    Update HW07_2__1.java

--- java/HW07/HW07_2__1.java
+++ java/HW07/HW07_2__1.java
@@ -5,9 +5,8 @@ public class HW07_2__1 {
             first = first + i + " ";
         }
         first = first.substring(0, first.length() - 1);
-        System.out.println(first);
 
-        for (int i = 2; i < first.length(); i = i + 2) {
+        for (int i = 0; i < first.length(); i = i + 2) {
             System.out.println(first.substring(0, first.length() - i));
         }
     }
